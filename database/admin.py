44a73042fa898165de19152777af45d0728663fb
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2024-04-05 20:06:12 +0200

    update admin

--- database/admin.py
+++ database/admin.py
@@ -33,6 +33,20 @@ class ParentAdmin(admin.ModelAdmin):
         queryset = self.model.objects.filter(recruit=request.user)
         return queryset
     
+    #def has_add_permission(self, request: HttpRequest) -> bool:
+    #    return True
+    #
+    #def has_view_permission(self, request: HttpRequest) -> bool:
+    #    return True
+    #
+    #def has_delete_permission(self, request: HttpRequest) -> bool:
+    #    return True
+    #
+    #def has_change_permission(self, request: HttpRequest) -> bool:
+    #    return True
+    
+    def has_module_permission(self, request: HttpRequest) -> bool:
+        return True
 
 
 @admin.register(Brother)
